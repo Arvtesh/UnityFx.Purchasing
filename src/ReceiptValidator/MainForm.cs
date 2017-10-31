25538e5505541f5378fb773f5891d91ddd276f36
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2017-10-31 18:59:45 +0200

    added server receipt validation methods to UnityFx.Purchasing

--- src/ReceiptValidator/MainForm.cs
+++ src/ReceiptValidator/MainForm.cs
@@ -26,52 +26,21 @@ namespace ReceiptValidator
 				}
 				else if (RadioPlatformIos.Checked)
 				{
-					TextResult.Text = await ValidateIos(TextReceipt.Text, false);
+					TextResult.Text = await UnityFx.Purchasing.Store.ValidatePurchaseReceiptIos(TextReceipt.Text, false);
 				}
 				else if (RadioPlatformIosSandbox.Checked)
 				{
-					TextResult.Text = await ValidateIos(TextReceipt.Text, true);
+					TextResult.Text = await UnityFx.Purchasing.Store.ValidatePurchaseReceiptIos(TextReceipt.Text, true);
 				}
 				else if (RadioPlatformAndroid.Checked)
 				{
-					TextResult.Text = await ValidateAndroid(TextReceipt.Text);
+					// TODO
 				}
 			}
 			catch (Exception ex)
 			{
 				TextResult.Text = "*** Exception: " + ex.ToString();
 			}
-		}
-
-		private async Task<string> ValidateIos(string receipt, bool sandbox)
-		{
-			var storeUrl = sandbox ? "https://sandbox.itunes.apple.com/verifyReceipt" : "https://buy.itunes.apple.com/verifyReceipt";
-			var json = string.Format("{{\"receipt-data\":\"{0}\"}}", receipt);
-			var ascii = new ASCIIEncoding();
-			var postBytes = Encoding.UTF8.GetBytes(json);
-
-			var request = WebRequest.Create(storeUrl);
-			request.Method = "POST";
-			request.ContentType = "application/json";
-			request.ContentLength = postBytes.Length;
-
-			using (var stream = await request.GetRequestStreamAsync())
-			{
-				await stream.WriteAsync(postBytes, 0, postBytes.Length);
-				await stream.FlushAsync();
-			}
-
-			var sendResponse = await request.GetResponseAsync();
-
-			using (var streamReader = new StreamReader(sendResponse.GetResponseStream()))
-			{
-				return await streamReader.ReadToEndAsync();
-			}
-		}
-
-		private async Task<string> ValidateAndroid(string receipt)
-		{
-			throw new NotImplementedException();
 		}
 	}
 }
--- src/UnityFx.Purchasing/Store.cs
+++ src/UnityFx.Purchasing/Store.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine.Purchasing.Extension;
 
 namespace UnityFx.Purchasing
@@ -29,6 +33,55 @@ namespace UnityFx.Purchasing
 			}
 
 			return new StoreService(string.Empty, purchasingModule, storeDelegate);
+		}
+
+		/// <summary>
+		/// Validates purchase receipt with iOS store and returns the store response string.
+		/// </summary>
+		/// <param name="receipt">Native iOS store receipt returned by the purchase operation.</param>
+		/// <param name="sandboxStore">If <c>true</c> sandbox store should be used.</param>
+		public static async Task<string> ValidatePurchaseReceiptIos(string receipt, bool sandboxStore = false)
+		{
+			var storeUrl = sandboxStore ? "https://sandbox.itunes.apple.com/verifyReceipt" : "https://buy.itunes.apple.com/verifyReceipt";
+			var json = string.Format("{{\"receipt-data\":\"{0}\"}}", receipt);
+			var ascii = new ASCIIEncoding();
+			var postBytes = Encoding.UTF8.GetBytes(json);
+
+			var request = WebRequest.Create(storeUrl);
+			request.Method = "POST";
+			request.ContentType = "application/json";
+			request.ContentLength = postBytes.Length;
+
+			using (var stream = await request.GetRequestStreamAsync())
+			{
+				await stream.WriteAsync(postBytes, 0, postBytes.Length);
+				await stream.FlushAsync();
+			}
+
+			var sendResponse = await request.GetResponseAsync();
+
+			using (var streamReader = new StreamReader(sendResponse.GetResponseStream()))
+			{
+				return await streamReader.ReadToEndAsync();
+			}
+		}
+
+		/// <summary>
+		/// Validates purchase receipt with Google Play store and returns the store response string.
+		/// </summary>
+		/// <param name="receipt">Native Google Play store receipt returned by the purchase operation.</param>
+		public static Task<string> ValidatePurchaseReceiptGooglePlay(string receipt)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Validates purchase receipt with Amazon store and returns the store response string.
+		/// </summary>
+		/// <param name="receipt">Native Amazon store receipt returned by the purchase operation.</param>
+		public static Task<string> ValidatePurchaseReceiptAmazon(string receipt)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
