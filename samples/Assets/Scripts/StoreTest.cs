1066c3b182205581b97f5d6cce7db8ba3bb68d44
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2017-11-30 00:18:30 +0200

    samples skeleton

---
+++ samples/Assets/Scripts/StoreTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
+
+namespace UnityFx.Purchasing.Tests
+{
+	public class StoreTest : MonoBehaviour, IStoreDelegate
+	{
+		#region MonoBehaviour
+
+		private void Awake()
+		{
+			var purchasingModule = StandardPurchasingModule.Instance();
+			purchasingModule.useFakeStoreAlways = true;
+			purchasingModule.useFakeStoreUIMode = FakeStoreUIMode.DeveloperUser;
+
+			var store = Store.CreateStore(purchasingModule, this);
+			store.TraceListeners.Add(new UnityTraceListener());
+			store.PurchaseAsync("r1");
+		}
+
+		#endregion
+
+		#region IStoreDelegate
+
+		public IDisposable BeginWait()
+		{
+			return null;
+		}
+
+		public Task<StoreConfig> GetStoreConfigAsync()
+		{
+			var products = new ProductDefinition[]
+			{
+				new ProductDefinition("1", ProductType.Consumable),
+				new ProductDefinition("2", ProductType.NonConsumable),
+				new ProductDefinition("3", ProductType.Subscription)
+			};
+
+			return Task.FromResult(new StoreConfig(products));
+		}
+
+		public Task<PurchaseValidationResult> ValidatePurchaseAsync(IStoreProduct product, StoreTransaction transactionInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ samples/Assets/Scripts/UnityTraceListener.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace UnityFx.Purchasing.Tests
+{
+	public class UnityTraceListener : TraceListener
+	{
+		public UnityTraceListener()
+			: base("UnityTraceListener")
+		{
+		}
+
+		public override void Write(string message)
+		{
+			UnityEngine.Debug.Log(message);
+		}
+
+		public override void WriteLine(string message)
+		{
+			UnityEngine.Debug.Log(message);
+		}
+
+		public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+		{
+			switch (eventType)
+			{
+				case TraceEventType.Warning:
+					UnityEngine.Debug.LogWarning("[<b>" + source + "</b>]: " + message);
+					break;
+
+				case TraceEventType.Error:
+				case TraceEventType.Critical:
+					UnityEngine.Debug.LogError("[<b>" + source + "</b>]: " + message);
+					break;
+
+				default:
+					UnityEngine.Debug.Log("[<b>" + source + "</b>]: " + message);
+					break;
+			}
+		}
+
+		public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+		{
+			switch (eventType)
+			{
+				case TraceEventType.Warning:
+					UnityEngine.Debug.LogWarning("[<b>" + source + "</b>]: " + data);
+					break;
+
+				case TraceEventType.Error:
+				case TraceEventType.Critical:
+					UnityEngine.Debug.LogError("[<b>" + source + "</b>]: " + data);
+					break;
+
+				default:
+					UnityEngine.Debug.Log("[<b>" + source + "</b>]: " + data);
+					break;
+			}
+		}
+	}
+}
