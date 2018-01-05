9a01a7db6b1e394ddd6a5efc88de95c21297e231
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-01-05 19:14:18 +0200

    added FailedPurchaseResult

--- src/UnityFx.Purchasing/Events/PurchaseFailedEventArgs.cs
+++ src/UnityFx.Purchasing/Events/PurchaseFailedEventArgs.cs
@@ -11,34 +11,24 @@ namespace UnityFx.Purchasing
 	public class PurchaseFailedEventArgs : EventArgs
 	{
 		/// <summary>
-		/// Returns the product identifier. Read only.
-		/// </summary>
-		public string ProductId { get; }
-
-		/// <summary>
 		/// Returns the purchase result. Read only.
 		/// </summary>
-		public PurchaseResult Result { get; }
+		public FailedPurchaseResult Result { get; }
 
 		/// <summary>
-		/// Returns an error that caused the purchase to fail. Read only.
+		/// Initializes a new instance of the <see cref="PurchaseFailedEventArgs"/> class.
 		/// </summary>
-		public StorePurchaseError Error { get; }
-
-		/// <summary>
-		/// Returns exception that caused the failure (if any). Read only.
-		/// </summary>
-		public Exception Exception { get; }
+		public PurchaseFailedEventArgs(FailedPurchaseResult result)
+		{
+			Result = result;
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PurchaseFailedEventArgs"/> class.
 		/// </summary>
 		public PurchaseFailedEventArgs(string productId, PurchaseResult result, StorePurchaseError error, Exception e)
 		{
-			ProductId = productId;
-			Result = result;
-			Error = error;
-			Exception = e;
+			Result = new FailedPurchaseResult(productId, result, error, e);
 		}
 	}
 }
---
+++ src/UnityFx.Purchasing/Store/FailedPurchaseResult.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace UnityFx.Purchasing
+{
+	/// <summary>
+	/// A store purchase result for failed purchases.
+	/// </summary>
+	public class FailedPurchaseResult : PurchaseResult
+	{
+		/// <summary>
+		/// Returns the product identifier. Read only.
+		/// </summary>
+		public string ProductId { get; }
+
+		/// <summary>
+		/// Returns an error that caused the purchase to fail. Read only.
+		/// </summary>
+		public StorePurchaseError Error { get; }
+
+		/// <summary>
+		/// Returns exception that caused the failure (if any). Read only.
+		/// </summary>
+		public Exception Exception { get; }
+
+		/// <summary>
+		/// Returns <see langword="true"/> if the purchase operation has failed; <see langword="false"/> otherwise. Read only.
+		/// </summary>
+		public bool IsCanceled => Error == StorePurchaseError.UserCanceled;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FailedPurchaseResult"/> class.
+		/// </summary>
+		public FailedPurchaseResult(string productId, PurchaseResult purchaseResult, StorePurchaseError error, Exception e)
+			: base(purchaseResult.TransactionInfo, purchaseResult.ValidationResult)
+		{
+			ProductId = productId;
+			Error = error;
+			Exception = e;
+		}
+	}
+}
