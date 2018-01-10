a60352110fcb8655af695512240dd2314cedc132
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-01-10 19:18:45 +0200

    added netstandard2.0 artifacts

--- src/Build.ps1
+++ src/Build.ps1
@@ -4,6 +4,7 @@ $configuration = $args[0]
 $packagesPath = Join-Path $scriptPath "..\temp\BuildTools"
 $binPath = Join-Path $scriptPath "..\bin"
 $binPath46 = Join-Path $binPath "net46"
+$binPathStandard20 = Join-Path $binPath "netstandard2.0"
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
 $nugetPath = Join-Path $packagesPath "nuget.exe"
 $gitversionPath = Join-Path $packagesPath "gitversion.commandline\tools\gitversion.exe"
@@ -19,6 +20,10 @@ if (!(Test-Path $packagesPath)) {
 
 if (!(Test-Path $binPath46)) {
 	New-Item $binPath46 -ItemType Directory
+}
+
+if (!(Test-Path $binPathStandard20)) {
+	New-Item $binPathStandard20 -ItemType Directory
 }
 
 # download nuget.exe if not present
@@ -48,10 +53,18 @@ if ($LastExitCode -ne 0) {
 }
 
 # publish build results to .\Build\Bin
-$filesToPublish =
+$filesToPublish46 =
 	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing\bin" (Join-Path $configuration "net46\UnityFx.Purchasing.dll"))),
 	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing\bin" (Join-Path $configuration "net46\UnityFx.Purchasing.xml"))),
 	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing.Validation\bin" (Join-Path $configuration "net46\UnityFx.Purchasing.Validation.dll"))),
 	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing.Validation\bin" (Join-Path $configuration "net46\UnityFx.Purchasing.Validation.xml")))
 
-Copy-Item -Path $filesToPublish -Destination $binPath46 -Force
+Copy-Item -Path $filesToPublish46 -Destination $binPath46 -Force
+
+$filesToPublishStandard20 =
+	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing\bin" (Join-Path $configuration "netstandard2.0\UnityFx.Purchasing.dll"))),
+	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing\bin" (Join-Path $configuration "netstandard2.0\UnityFx.Purchasing.xml"))),
+	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing.Validation\bin" (Join-Path $configuration "netstandard1.3\UnityFx.Purchasing.Validation.dll"))),
+	(Join-Path $scriptPath (Join-Path "UnityFx.Purchasing.Validation\bin" (Join-Path $configuration "netstandard1.3\UnityFx.Purchasing.Validation.xml")))
+
+Copy-Item -Path $filesToPublishStandard20 -Destination $binPathStandard20 -Force
