e464d7e6ab0263e19d3a626076cc6586cdfdb535
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-01-24 12:45:45 +0200

    added store service tests

---
+++ src/UnityFx.Purchasing.Tests/Helpers/TestStoreService.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using UnityEngine.Purchasing;
+
+namespace UnityFx.Purchasing.Tests
+{
+	internal class TestStoreService : StoreService
+	{
+		public TestStoreService()
+			: base(null, new TestPurchasingModule())
+		{
+		}
+
+		protected override Task<StoreConfig> GetStoreConfigAsync()
+		{
+			var products = new ProductDefinition[]
+			{
+				new ProductDefinition("test_product_1", ProductType.Consumable),
+				new ProductDefinition("test_product_2", ProductType.NonConsumable),
+				new ProductDefinition("test_product_3", ProductType.Subscription)
+			};
+
+			var config = new StoreConfig(products);
+			return Task.FromResult(config);
+		}
+
+		protected override bool IsPlatformSupported()
+		{
+			return true;
+		}
+	}
+}
---
+++ src/UnityFx.Purchasing.Tests/Tests/StoreTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace UnityFx.Purchasing.Tests
+{
+	public class StoreTests
+	{
+		[Fact]
+		public void StoreCreationSucceeds()
+		{
+			var store = new TestStoreService();
+			store.InitializeAsync(null);
+		}
+	}
+}
